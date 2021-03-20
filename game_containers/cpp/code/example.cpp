0360d6ab57dd34b1ee68ee7f17b659c84a41df84
Seth Hamilton <sethhamilton@pop-os.localdomain>
2021-03-20 13:37:41 -0500

    started game containers

---
+++ game_containers/cpp/code/example.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    int board[9];
+
+    for (int i = 0; i < 9; i++)
+        board[i] = 0;
+
+    
+}
---
+++ game_containers/python/code/agent.py
@@ -0,0 +1,6 @@
+import torch
+import tensorflow
+import numpy as np
+import pandas as pd
+
+print("HOWDY!")
